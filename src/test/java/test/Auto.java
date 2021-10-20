f202600331405cfb5840fcfc5e7b82a6f6a97e28
alejandrons <80337888+alejandrons@users.noreply.github.com>
2021-10-19 23:58:09 -0500

    Solucion

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,23 @@
+
+package test;
+
+
+public class Auto {
+    String modelo,marca;
+    int precio,registro;
+    Asiento[] asientos;
+    Motor motor;
+    static int cantidadCreados;
+    
+    int cantidadAsientos(){
+        return asientos.length;
+    }
+    String verificarIntegridad(){
+        for(Asiento a : asientos){
+            if(a.registro != motor.registro && a.registro != registro){
+                return "Las piezas no son originales";
+            }
+        }
+        return "Auto original";
+    }
+}
